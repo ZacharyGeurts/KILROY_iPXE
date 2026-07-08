97934706866504f0dac32a398300061fadaf32cd
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:00:33 -0500

    Add files via upload

---
+++ src/bin-x86_64-efi/deps/drivers/bus/cdc.c.d
@@ -0,0 +1,69 @@
+cdc_DEPS += drivers/bus/cdc.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h include/stddef.h include/stdint.h \
+ arch/x86_64/include/bits/stdint.h include/ipxe/usb.h include/byteswap.h \
+ include/endian.h arch/x86/include/bits/endian.h \
+ arch/x86_64/include/bits/byteswap.h include/ipxe/list.h include/assert.h \
+ include/ipxe/device.h include/ipxe/tables.h include/ipxe/process.h \
+ include/ipxe/refcnt.h include/ipxe/iobuf.h include/ipxe/dma.h \
+ include/ipxe/api.h include/ipxe/io.h include/ipxe/iomap.h config/ioapi.h \
+ config/defaults.h config/defaults/efi.h config/named.h \
+ config/local/ioapi.h include/ipxe/iomap_virt.h include/ipxe/uaccess.h \
+ include/ipxe/virt_offset.h include/bits/virt_offset.h \
+ include/ipxe/linux/linux_uaccess.h include/bits/uaccess.h \
+ arch/x86/include/bits/iomap.h arch/x86/include/ipxe/iomap_pages.h \
+ arch/x86/include/bits/io.h arch/x86/include/ipxe/x86_io.h \
+ include/ipxe/malloc.h include/stdlib.h include/valgrind/memcheck.h \
+ include/valgrind/valgrind.h include/stdarg.h include/ipxe/umalloc.h \
+ config/umalloc.h config/local/umalloc.h include/ipxe/uheap.h \
+ include/ipxe/efi/efi_umalloc.h include/ipxe/linux/linux_umalloc.h \
+ include/bits/umalloc.h include/bits/dma.h include/ipxe/cdc.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+include/stddef.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/ipxe/usb.h:
+include/byteswap.h:
+include/endian.h:
+arch/x86/include/bits/endian.h:
+arch/x86_64/include/bits/byteswap.h:
+include/ipxe/list.h:
+include/assert.h:
+include/ipxe/device.h:
+include/ipxe/tables.h:
+include/ipxe/process.h:
+include/ipxe/refcnt.h:
+include/ipxe/iobuf.h:
+include/ipxe/dma.h:
+include/ipxe/api.h:
+include/ipxe/io.h:
+include/ipxe/iomap.h:
+config/ioapi.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/named.h:
+config/local/ioapi.h:
+include/ipxe/iomap_virt.h:
+include/ipxe/uaccess.h:
+include/ipxe/virt_offset.h:
+include/bits/virt_offset.h:
+include/ipxe/linux/linux_uaccess.h:
+include/bits/uaccess.h:
+arch/x86/include/bits/iomap.h:
+arch/x86/include/ipxe/iomap_pages.h:
+arch/x86/include/bits/io.h:
+arch/x86/include/ipxe/x86_io.h:
+include/ipxe/malloc.h:
+include/stdlib.h:
+include/valgrind/memcheck.h:
+include/valgrind/valgrind.h:
+include/stdarg.h:
+include/ipxe/umalloc.h:
+config/umalloc.h:
+config/local/umalloc.h:
+include/ipxe/uheap.h:
+include/ipxe/efi/efi_umalloc.h:
+include/ipxe/linux/linux_umalloc.h:
+include/bits/umalloc.h:
+include/bits/dma.h:
+include/ipxe/cdc.h:
