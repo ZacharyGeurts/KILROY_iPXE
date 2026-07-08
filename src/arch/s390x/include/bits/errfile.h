535807cfb7d7659ae6479ead59837fbe08fd196a
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:31:11 -0500

    Add files via upload
    Signed-off-by: Zachary Robert Geurts <gzac5314@gmail.com>

---
+++ src/arch/s390x/include/bits/errfile.h
@@ -0,0 +1,22 @@
+#ifndef _BITS_ERRFILE_H
+#define _BITS_ERRFILE_H
+
+/** @file
+ *
+ * S/390 error file identifiers
+ *
+ */
+
+FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
+
+/**
+ * @addtogroup errfile Error file identifiers
+ * @{
+ */
+
+#define ERRFILE_prno		( ERRFILE_ARCH | ERRFILE_CORE | 0x00010000 )
+#define ERRFILE_tod		( ERRFILE_ARCH | ERRFILE_CORE | 0x00020000 )
+
+/** @} */
+
+#endif /* _BITS_ERRFILE_H */
