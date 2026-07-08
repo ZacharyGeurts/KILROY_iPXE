60957982bc1c18845e4e48de280469f30748b236
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:04:31 -0500

    Add files via upload

---
+++ src/bin-x86_64-efi/deps/drivers/net/vxge/vxge_config.c.d
@@ -0,0 +1,103 @@
+vxge_config_DEPS += drivers/net/vxge/vxge_config.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h include/stdlib.h include/stdint.h \
+ arch/x86_64/include/bits/stdint.h include/assert.h include/stdio.h \
+ include/stdarg.h include/string.h include/stddef.h \
+ arch/x86/include/bits/string.h include/ipxe/malloc.h include/ipxe/list.h \
+ include/ipxe/tables.h include/valgrind/memcheck.h \
+ include/valgrind/valgrind.h include/ipxe/pci.h include/ipxe/device.h \
+ include/ipxe/dma.h include/ipxe/api.h include/ipxe/io.h \
+ include/ipxe/iomap.h config/ioapi.h config/defaults.h \
+ config/defaults/efi.h config/named.h config/local/ioapi.h \
+ include/ipxe/iomap_virt.h include/ipxe/uaccess.h \
+ include/ipxe/virt_offset.h include/bits/virt_offset.h \
+ include/ipxe/linux/linux_uaccess.h include/bits/uaccess.h \
+ arch/x86/include/bits/iomap.h arch/x86/include/ipxe/iomap_pages.h \
+ arch/x86/include/bits/io.h arch/x86/include/ipxe/x86_io.h \
+ include/ipxe/umalloc.h config/umalloc.h config/local/umalloc.h \
+ include/ipxe/uheap.h include/ipxe/efi/efi_umalloc.h \
+ include/ipxe/linux/linux_umalloc.h include/bits/umalloc.h \
+ include/bits/dma.h include/ipxe/pci_io.h include/ipxe/null_pci.h \
+ include/ipxe/ecam_io.h include/ipxe/pcicloud.h \
+ include/ipxe/efi/efi_pci_api.h include/ipxe/linux/linux_pci.h \
+ arch/x86/include/bits/pci_io.h arch/x86/include/ipxe/pcibios.h \
+ arch/x86/include/ipxe/pcidirect.h include/ipxe/iobuf.h \
+ include/ipxe/ethernet.h include/ipxe/netdevice.h include/ipxe/refcnt.h \
+ include/ipxe/settings.h include/ipxe/interface.h include/ipxe/retry.h \
+ include/byteswap.h include/endian.h arch/x86/include/bits/endian.h \
+ arch/x86_64/include/bits/byteswap.h drivers/net/vxge/vxge_traffic.h \
+ include/ipxe/if_ether.h drivers/net/vxge/vxge_reg.h \
+ drivers/net/vxge/vxge_version.h drivers/net/vxge/vxge_config.h \
+ drivers/net/vxge/vxge_main.h include/unistd.h include/ipxe/timer.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+include/stdlib.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/assert.h:
+include/stdio.h:
+include/stdarg.h:
+include/string.h:
+include/stddef.h:
+arch/x86/include/bits/string.h:
+include/ipxe/malloc.h:
+include/ipxe/list.h:
+include/ipxe/tables.h:
+include/valgrind/memcheck.h:
+include/valgrind/valgrind.h:
+include/ipxe/pci.h:
+include/ipxe/device.h:
+include/ipxe/dma.h:
+include/ipxe/api.h:
+include/ipxe/io.h:
+include/ipxe/iomap.h:
+config/ioapi.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/named.h:
+config/local/ioapi.h:
+include/ipxe/iomap_virt.h:
+include/ipxe/uaccess.h:
+include/ipxe/virt_offset.h:
+include/bits/virt_offset.h:
+include/ipxe/linux/linux_uaccess.h:
+include/bits/uaccess.h:
+arch/x86/include/bits/iomap.h:
+arch/x86/include/ipxe/iomap_pages.h:
+arch/x86/include/bits/io.h:
+arch/x86/include/ipxe/x86_io.h:
+include/ipxe/umalloc.h:
+config/umalloc.h:
+config/local/umalloc.h:
+include/ipxe/uheap.h:
+include/ipxe/efi/efi_umalloc.h:
+include/ipxe/linux/linux_umalloc.h:
+include/bits/umalloc.h:
+include/bits/dma.h:
+include/ipxe/pci_io.h:
+include/ipxe/null_pci.h:
+include/ipxe/ecam_io.h:
+include/ipxe/pcicloud.h:
+include/ipxe/efi/efi_pci_api.h:
+include/ipxe/linux/linux_pci.h:
+arch/x86/include/bits/pci_io.h:
+arch/x86/include/ipxe/pcibios.h:
+arch/x86/include/ipxe/pcidirect.h:
+include/ipxe/iobuf.h:
+include/ipxe/ethernet.h:
+include/ipxe/netdevice.h:
+include/ipxe/refcnt.h:
+include/ipxe/settings.h:
+include/ipxe/interface.h:
+include/ipxe/retry.h:
+include/byteswap.h:
+include/endian.h:
+arch/x86/include/bits/endian.h:
+arch/x86_64/include/bits/byteswap.h:
+drivers/net/vxge/vxge_traffic.h:
+include/ipxe/if_ether.h:
+drivers/net/vxge/vxge_reg.h:
+drivers/net/vxge/vxge_version.h:
+drivers/net/vxge/vxge_config.h:
+drivers/net/vxge/vxge_main.h:
+include/unistd.h:
+include/ipxe/timer.h:
