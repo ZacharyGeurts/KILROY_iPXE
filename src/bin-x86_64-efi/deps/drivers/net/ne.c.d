33fb5064a3221beff10aa2a47f9f5d3bd8d1fc62
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:05:23 -0500

    Add files via upload

---
+++ src/bin-x86_64-efi/deps/drivers/net/ne.c.d
@@ -0,0 +1,4 @@
+ne_DEPS += drivers/net/ne.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
---
+++ src/bin-x86_64-efi/deps/drivers/net/wd.c.d
@@ -0,0 +1,4 @@
+wd_DEPS += drivers/net/wd.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
