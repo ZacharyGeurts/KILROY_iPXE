d5ad3b14ed74c5e6553fe426f913d30f8d5118f2
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:08:46 -0500

    Add files via upload

---
+++ src/bin-x86_64-efi/deps/crypto/mishmash/cmd_sha224.c.d
@@ -0,0 +1,40 @@
+cmd_sha224_DEPS += crypto/mishmash/cmd_sha224.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h include/ipxe/sha256.h \
+ include/stdint.h arch/x86_64/include/bits/stdint.h include/endian.h \
+ arch/x86/include/bits/endian.h include/ipxe/crypto.h include/stddef.h \
+ include/assert.h include/ipxe/asn1.h include/stdarg.h include/time.h \
+ include/sys/time.h include/ipxe/time.h include/ipxe/api.h config/time.h \
+ config/defaults.h config/defaults/efi.h config/local/time.h \
+ include/ipxe/null_time.h include/ipxe/efi/efi_time.h \
+ include/ipxe/linux/linux_time.h arch/x86/include/bits/time.h \
+ arch/x86/include/ipxe/rtc_time.h include/ipxe/tables.h \
+ include/ipxe/mdhash.h include/hci/digest_cmd.h include/ipxe/command.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+include/ipxe/sha256.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/endian.h:
+arch/x86/include/bits/endian.h:
+include/ipxe/crypto.h:
+include/stddef.h:
+include/assert.h:
+include/ipxe/asn1.h:
+include/stdarg.h:
+include/time.h:
+include/sys/time.h:
+include/ipxe/time.h:
+include/ipxe/api.h:
+config/time.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/local/time.h:
+include/ipxe/null_time.h:
+include/ipxe/efi/efi_time.h:
+include/ipxe/linux/linux_time.h:
+arch/x86/include/bits/time.h:
+arch/x86/include/ipxe/rtc_time.h:
+include/ipxe/tables.h:
+include/ipxe/mdhash.h:
+include/hci/digest_cmd.h:
+include/ipxe/command.h:
---
+++ src/bin-x86_64-efi/deps/crypto/mishmash/oid_sha384.c.d
@@ -0,0 +1,38 @@
+oid_sha384_DEPS += crypto/mishmash/oid_sha384.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h include/ipxe/sha512.h \
+ include/stdint.h arch/x86_64/include/bits/stdint.h include/endian.h \
+ arch/x86/include/bits/endian.h include/ipxe/crypto.h include/stddef.h \
+ include/assert.h include/ipxe/asn1.h include/stdarg.h include/time.h \
+ include/sys/time.h include/ipxe/time.h include/ipxe/api.h config/time.h \
+ config/defaults.h config/defaults/efi.h config/local/time.h \
+ include/ipxe/null_time.h include/ipxe/efi/efi_time.h \
+ include/ipxe/linux/linux_time.h arch/x86/include/bits/time.h \
+ arch/x86/include/ipxe/rtc_time.h include/ipxe/tables.h \
+ include/ipxe/mdhash.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+include/ipxe/sha512.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/endian.h:
+arch/x86/include/bits/endian.h:
+include/ipxe/crypto.h:
+include/stddef.h:
+include/assert.h:
+include/ipxe/asn1.h:
+include/stdarg.h:
+include/time.h:
+include/sys/time.h:
+include/ipxe/time.h:
+include/ipxe/api.h:
+config/time.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/local/time.h:
+include/ipxe/null_time.h:
+include/ipxe/efi/efi_time.h:
+include/ipxe/linux/linux_time.h:
+arch/x86/include/bits/time.h:
+arch/x86/include/ipxe/rtc_time.h:
+include/ipxe/tables.h:
+include/ipxe/mdhash.h:
