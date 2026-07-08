48dcabef588c0f7b2f696406b423215a01ee6fc1
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 21:51:34 -0500

    Add files via upload

---
+++ src/core/errno.c
@@ -0,0 +1,21 @@
+#include <errno.h>
+
+FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
+FILE_SECBOOT ( PERMITTED );
+
+/** @file
+ *
+ * Error codes
+ *
+ * This file provides the global variable #errno.
+ *
+ */
+
+/**
+ * Global "last error" number.
+ *
+ * This is valid only when a function has just returned indicating a
+ * failure.
+ *
+ */
+int errno;
