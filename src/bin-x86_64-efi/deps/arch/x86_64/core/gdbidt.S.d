73315b64181bada2dab7992b4c8ff12898d3e77f
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:23:17 -0500

    Add files via upload
    Signed-off-by: Zachary Robert Geurts <gzac5314@gmail.com>

---
+++ src/bin-x86_64-efi/deps/arch/x86_64/core/gdbidt.S.d
@@ -0,0 +1,4 @@
+gdbidt_DEPS += arch/x86_64/core/gdbidt.S include/compiler.h \
+ arch/x86_64/include/bits/compiler.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
---
+++ src/bin-x86_64-efi/deps/arch/x86_64/core/setjmp.S.d
@@ -0,0 +1,4 @@
+setjmp_DEPS += arch/x86_64/core/setjmp.S include/compiler.h \
+ arch/x86_64/include/bits/compiler.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
