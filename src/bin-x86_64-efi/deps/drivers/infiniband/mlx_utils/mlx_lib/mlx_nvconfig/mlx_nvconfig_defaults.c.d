50e805fe8f320dcf250e4651e089405ba33a2055
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:01:06 -0500

    Add files via upload

---
+++ src/bin-x86_64-efi/deps/drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/mlx_nvconfig_defaults.c.d
@@ -0,0 +1,109 @@
+mlx_nvconfig_defaults_DEPS += \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/mlx_nvconfig_defaults.c \
+ include/compiler.h arch/x86_64/include/bits/compiler.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/mlx_nvconfig.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/mlx_reg_access.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_icmd.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_utils.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_logging.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/../../../mlx_utils_flexboot/include/mlx_logging_priv.h \
+ include/stdio.h include/stdint.h arch/x86_64/include/bits/stdint.h \
+ include/stdarg.h include/compiler.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_types.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/../../../mlx_utils_flexboot/include/mlx_types_priv.h \
+ include/ipxe/pci.h include/ipxe/device.h include/ipxe/list.h \
+ include/stddef.h include/assert.h include/ipxe/tables.h \
+ include/ipxe/dma.h include/ipxe/api.h include/ipxe/io.h \
+ include/ipxe/iomap.h config/ioapi.h config/defaults.h \
+ config/defaults/efi.h config/named.h config/local/ioapi.h \
+ include/ipxe/iomap_virt.h include/ipxe/uaccess.h \
+ include/ipxe/virt_offset.h include/bits/virt_offset.h \
+ include/ipxe/linux/linux_uaccess.h include/bits/uaccess.h \
+ arch/x86/include/bits/iomap.h arch/x86/include/ipxe/iomap_pages.h \
+ arch/x86/include/bits/io.h arch/x86/include/ipxe/x86_io.h \
+ include/ipxe/malloc.h include/stdlib.h include/valgrind/memcheck.h \
+ include/valgrind/valgrind.h include/ipxe/umalloc.h config/umalloc.h \
+ config/local/umalloc.h include/ipxe/uheap.h \
+ include/ipxe/efi/efi_umalloc.h include/ipxe/linux/linux_umalloc.h \
+ include/bits/umalloc.h include/bits/dma.h include/ipxe/pci_io.h \
+ include/ipxe/null_pci.h include/ipxe/ecam_io.h include/ipxe/pcicloud.h \
+ include/ipxe/efi/efi_pci_api.h include/ipxe/linux/linux_pci.h \
+ arch/x86/include/bits/pci_io.h arch/x86/include/ipxe/pcibios.h \
+ arch/x86/include/ipxe/pcidirect.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../../include/public/mlx_utils.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_memory.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_utils.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_bail.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_types.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/mlx_nvconfig_defaults.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/mlx_nvconfig_prm.h \
+ drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../../include/public/mlx_types.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/mlx_nvconfig.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/mlx_reg_access.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_icmd.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_utils.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_logging.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/../../../mlx_utils_flexboot/include/mlx_logging_priv.h:
+include/stdio.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/stdarg.h:
+include/compiler.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/mlx_types.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../mlx_reg_access/../../include/public/../../../mlx_utils_flexboot/include/mlx_types_priv.h:
+include/ipxe/pci.h:
+include/ipxe/device.h:
+include/ipxe/list.h:
+include/stddef.h:
+include/assert.h:
+include/ipxe/tables.h:
+include/ipxe/dma.h:
+include/ipxe/api.h:
+include/ipxe/io.h:
+include/ipxe/iomap.h:
+config/ioapi.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/named.h:
+config/local/ioapi.h:
+include/ipxe/iomap_virt.h:
+include/ipxe/uaccess.h:
+include/ipxe/virt_offset.h:
+include/bits/virt_offset.h:
+include/ipxe/linux/linux_uaccess.h:
+include/bits/uaccess.h:
+arch/x86/include/bits/iomap.h:
+arch/x86/include/ipxe/iomap_pages.h:
+arch/x86/include/bits/io.h:
+arch/x86/include/ipxe/x86_io.h:
+include/ipxe/malloc.h:
+include/stdlib.h:
+include/valgrind/memcheck.h:
+include/valgrind/valgrind.h:
+include/ipxe/umalloc.h:
+config/umalloc.h:
+config/local/umalloc.h:
+include/ipxe/uheap.h:
+include/ipxe/efi/efi_umalloc.h:
+include/ipxe/linux/linux_umalloc.h:
+include/bits/umalloc.h:
+include/bits/dma.h:
+include/ipxe/pci_io.h:
+include/ipxe/null_pci.h:
+include/ipxe/ecam_io.h:
+include/ipxe/pcicloud.h:
+include/ipxe/efi/efi_pci_api.h:
+include/ipxe/linux/linux_pci.h:
+arch/x86/include/bits/pci_io.h:
+arch/x86/include/ipxe/pcibios.h:
+arch/x86/include/ipxe/pcidirect.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../../include/public/mlx_utils.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_memory.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_utils.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_bail.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../include/public/mlx_types.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/mlx_nvconfig_defaults.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/mlx_nvconfig_prm.h:
+drivers/infiniband/mlx_utils/mlx_lib/mlx_nvconfig/../../mlx_lib/mlx_nvconfig/../../include/public/mlx_types.h:
---
+++ src/bin-x86_64-efi/deps/drivers/infiniband/mlx_utils_flexboot/src/mlx_memory_priv.c.d
@@ -0,0 +1,97 @@
+mlx_memory_priv_DEPS += \
+ drivers/infiniband/mlx_utils_flexboot/src/mlx_memory_priv.c \
+ include/compiler.h arch/x86_64/include/bits/compiler.h \
+ include/ipxe/malloc.h include/stdint.h arch/x86_64/include/bits/stdint.h \
+ include/stdlib.h include/assert.h include/ipxe/list.h include/stddef.h \
+ include/ipxe/tables.h include/valgrind/memcheck.h \
+ include/valgrind/valgrind.h include/stdarg.h include/string.h \
+ arch/x86/include/bits/string.h include/byteswap.h include/endian.h \
+ arch/x86/include/bits/endian.h arch/x86_64/include/bits/byteswap.h \
+ include/ipxe/io.h include/ipxe/api.h include/ipxe/iomap.h config/ioapi.h \
+ config/defaults.h config/defaults/efi.h config/named.h \
+ config/local/ioapi.h include/ipxe/iomap_virt.h include/ipxe/uaccess.h \
+ include/ipxe/virt_offset.h include/bits/virt_offset.h \
+ include/ipxe/linux/linux_uaccess.h include/bits/uaccess.h \
+ arch/x86/include/bits/iomap.h arch/x86/include/ipxe/iomap_pages.h \
+ arch/x86/include/bits/io.h arch/x86/include/ipxe/x86_io.h \
+ drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/mlx_memory_priv.h \
+ drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/mlx_utils.h \
+ drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/mlx_logging.h \
+ drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/../../../mlx_utils_flexboot/include/mlx_logging_priv.h \
+ include/stdio.h include/compiler.h \
+ drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/mlx_types.h \
+ drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/../../../mlx_utils_flexboot/include/mlx_types_priv.h \
+ include/ipxe/pci.h include/ipxe/device.h include/ipxe/dma.h \
+ include/ipxe/umalloc.h config/umalloc.h config/local/umalloc.h \
+ include/ipxe/uheap.h include/ipxe/efi/efi_umalloc.h \
+ include/ipxe/linux/linux_umalloc.h include/bits/umalloc.h \
+ include/bits/dma.h include/ipxe/pci_io.h include/ipxe/null_pci.h \
+ include/ipxe/ecam_io.h include/ipxe/pcicloud.h \
+ include/ipxe/efi/efi_pci_api.h include/ipxe/linux/linux_pci.h \
+ arch/x86/include/bits/pci_io.h arch/x86/include/ipxe/pcibios.h \
+ arch/x86/include/ipxe/pcidirect.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+include/ipxe/malloc.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/stdlib.h:
+include/assert.h:
+include/ipxe/list.h:
+include/stddef.h:
+include/ipxe/tables.h:
+include/valgrind/memcheck.h:
+include/valgrind/valgrind.h:
+include/stdarg.h:
+include/string.h:
+arch/x86/include/bits/string.h:
+include/byteswap.h:
+include/endian.h:
+arch/x86/include/bits/endian.h:
+arch/x86_64/include/bits/byteswap.h:
+include/ipxe/io.h:
+include/ipxe/api.h:
+include/ipxe/iomap.h:
+config/ioapi.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/named.h:
+config/local/ioapi.h:
+include/ipxe/iomap_virt.h:
+include/ipxe/uaccess.h:
+include/ipxe/virt_offset.h:
+include/bits/virt_offset.h:
+include/ipxe/linux/linux_uaccess.h:
+include/bits/uaccess.h:
+arch/x86/include/bits/iomap.h:
+arch/x86/include/ipxe/iomap_pages.h:
+arch/x86/include/bits/io.h:
+arch/x86/include/ipxe/x86_io.h:
+drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/mlx_memory_priv.h:
+drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/mlx_utils.h:
+drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/mlx_logging.h:
+drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/../../../mlx_utils_flexboot/include/mlx_logging_priv.h:
+include/stdio.h:
+include/compiler.h:
+drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/mlx_types.h:
+drivers/infiniband/mlx_utils_flexboot/src/../../mlx_utils/include/private/../../../mlx_utils/include/public/../../../mlx_utils_flexboot/include/mlx_types_priv.h:
+include/ipxe/pci.h:
+include/ipxe/device.h:
+include/ipxe/dma.h:
+include/ipxe/umalloc.h:
+config/umalloc.h:
+config/local/umalloc.h:
+include/ipxe/uheap.h:
+include/ipxe/efi/efi_umalloc.h:
+include/ipxe/linux/linux_umalloc.h:
+include/bits/umalloc.h:
+include/bits/dma.h:
+include/ipxe/pci_io.h:
+include/ipxe/null_pci.h:
+include/ipxe/ecam_io.h:
+include/ipxe/pcicloud.h:
+include/ipxe/efi/efi_pci_api.h:
+include/ipxe/linux/linux_pci.h:
+arch/x86/include/bits/pci_io.h:
+arch/x86/include/ipxe/pcibios.h:
+arch/x86/include/ipxe/pcidirect.h:
