32b8cd374ca54b09dcd7b82f912bf755d8ad47de
Zachary Robert Geurts <gzac5314@gmail.com>
2026-07-07 22:03:42 -0500

    Add files via upload

---
+++ src/bin-x86_64-efi/deps/drivers/net/ath/ath_hw.c.d
@@ -0,0 +1,83 @@
+ath_hw_DEPS += drivers/net/ath/ath_hw.c include/compiler.h \
+ arch/x86_64/include/bits/compiler.h include/ipxe/io.h include/stdint.h \
+ arch/x86_64/include/bits/stdint.h include/ipxe/api.h \
+ include/ipxe/iomap.h config/ioapi.h config/defaults.h \
+ config/defaults/efi.h config/named.h config/local/ioapi.h \
+ include/ipxe/iomap_virt.h include/stddef.h include/ipxe/uaccess.h \
+ include/ipxe/virt_offset.h include/bits/virt_offset.h \
+ include/ipxe/linux/linux_uaccess.h include/bits/uaccess.h \
+ arch/x86/include/bits/iomap.h arch/x86/include/ipxe/iomap_pages.h \
+ arch/x86/include/bits/io.h arch/x86/include/ipxe/x86_io.h \
+ drivers/net/ath/ath.h include/unistd.h include/stdarg.h \
+ include/ipxe/timer.h include/ipxe/tables.h include/string.h \
+ arch/x86/include/bits/string.h include/ipxe/net80211.h \
+ include/ipxe/process.h include/ipxe/list.h include/assert.h \
+ include/ipxe/refcnt.h include/ipxe/ieee80211.h include/ipxe/if_ether.h \
+ include/endian.h arch/x86/include/bits/endian.h include/ipxe/iobuf.h \
+ include/ipxe/dma.h include/ipxe/malloc.h include/stdlib.h \
+ include/valgrind/memcheck.h include/valgrind/valgrind.h \
+ include/ipxe/umalloc.h config/umalloc.h config/local/umalloc.h \
+ include/ipxe/uheap.h include/ipxe/efi/efi_umalloc.h \
+ include/ipxe/linux/linux_umalloc.h include/bits/umalloc.h \
+ include/bits/dma.h include/ipxe/netdevice.h include/ipxe/settings.h \
+ include/ipxe/interface.h include/ipxe/retry.h include/ipxe/rc80211.h \
+ drivers/net/ath/reg.h
+include/compiler.h:
+arch/x86_64/include/bits/compiler.h:
+include/ipxe/io.h:
+include/stdint.h:
+arch/x86_64/include/bits/stdint.h:
+include/ipxe/api.h:
+include/ipxe/iomap.h:
+config/ioapi.h:
+config/defaults.h:
+config/defaults/efi.h:
+config/named.h:
+config/local/ioapi.h:
+include/ipxe/iomap_virt.h:
+include/stddef.h:
+include/ipxe/uaccess.h:
+include/ipxe/virt_offset.h:
+include/bits/virt_offset.h:
+include/ipxe/linux/linux_uaccess.h:
+include/bits/uaccess.h:
+arch/x86/include/bits/iomap.h:
+arch/x86/include/ipxe/iomap_pages.h:
+arch/x86/include/bits/io.h:
+arch/x86/include/ipxe/x86_io.h:
+drivers/net/ath/ath.h:
+include/unistd.h:
+include/stdarg.h:
+include/ipxe/timer.h:
+include/ipxe/tables.h:
+include/string.h:
+arch/x86/include/bits/string.h:
+include/ipxe/net80211.h:
+include/ipxe/process.h:
+include/ipxe/list.h:
+include/assert.h:
+include/ipxe/refcnt.h:
+include/ipxe/ieee80211.h:
+include/ipxe/if_ether.h:
+include/endian.h:
+arch/x86/include/bits/endian.h:
+include/ipxe/iobuf.h:
+include/ipxe/dma.h:
+include/ipxe/malloc.h:
+include/stdlib.h:
+include/valgrind/memcheck.h:
+include/valgrind/valgrind.h:
+include/ipxe/umalloc.h:
+config/umalloc.h:
+config/local/umalloc.h:
+include/ipxe/uheap.h:
+include/ipxe/efi/efi_umalloc.h:
+include/ipxe/linux/linux_umalloc.h:
+include/bits/umalloc.h:
+include/bits/dma.h:
+include/ipxe/netdevice.h:
+include/ipxe/settings.h:
+include/ipxe/interface.h:
+include/ipxe/retry.h:
+include/ipxe/rc80211.h:
+drivers/net/ath/reg.h:
